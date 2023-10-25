42389668fb2904e3dbab742ca667066f8b12bd69
Martin <martinvicknair@gmail.com>
2023-10-25 12:22:35 -0600

    refactor/documentation

--- webmaster/download-hack.php
+++ webmaster/download-hack.php
@@ -1,7 +1,11 @@
 <?php
-
-$hack_count = @file_get_contents('hack-count.txt');
+/**
+ * Reads the current download count from the file hack-count.txt, increments it by 1, and writes it back to the file.
+ * Then redirects the user to the infection.php page for payload download.
+ */
+$hack_count = (int)file_get_contents('hack-count.txt');
 $hack_count++;
-@file_put_contents('hack-count.txt', $hack_count);
+file_put_contents('hack-count.txt', $hack_count);
 
-header('Location: https://withersteen.zapto.org/webmaster/infection.php'); // redirect to the real file to be downloaded
+header('Location: https://withersteen.zapto.org/webmaster/infection.php');
+
--- webmaster/infection.php
+++ webmaster/infection.php
@@ -1,22 +1,26 @@
 <?php
-    // Fetch the file info.
-    $filePath = 'infectado.jpg';
+/**
+ * This script fetches the file info of the payload file 'infectado.jpg' and outputs it as a downloadable attachment.
+ * If the file does not exist, it will output an error message.
+ */
 
-    if(file_exists($filePath)) {
-        $fileName = basename($filePath);
-        $fileSize = filesize($filePath);
+// Fetch the file info.
+$filePath = 'infectado.jpg';
 
-        // Output headers.
-        header("Cache-Control: private");
-        header("Content-Type: application/stream");
-        header("Content-Length: ".$fileSize);
-        header("Content-Disposition: attachment; filename=".$fileName);
+if (file_exists($filePath)) {
+    $fileName = basename($filePath);
+    $fileSize = filesize($filePath);
 
-        // Output file.
-        readfile ($filePath);
-        exit();
-    }
-    else {
-        die('The provided file path is not valid.');
-    }
+    // Output headers.
+    header("Cache-Control: private");
+    header("Content-Type: application/stream");
+    header("Content-Length: " . $fileSize);
+    header("Content-Disposition: attachment; filename=" . $fileName);
+
+    // Output file.
+    readfile($filePath);
+    exit();
+} else {
+    die('The provided file path is not valid.');
+}
 ?>
